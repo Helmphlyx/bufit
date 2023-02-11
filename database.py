8ce6e1eb9776c936796aeaf340e1a48d59aabef3
Helmphlyx <btruong413@gmail.com>
2023-02-11 16:19:25 -0500

    initialize data from external API

--- database.py
+++ database.py
@@ -1,51 +1,75 @@
-import sqlite3
+import re
+import requests
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlite_utilities import SqliteUtilites
+from settings import settings
 
+DATABASE_NAME = settings.DATABASE_NAME
 db = SQLAlchemy()
 
 
 def init_db():
-    db_utils = SqliteUtilites("database.db")
+    db_utils = SqliteUtilites(DATABASE_NAME)
     db_utils.execute_from_file("db/sql/create_tables.sql")
+    init_muscle_table()
+    init_exercise_table()
 
-    # SQL to initialize database tables
-    connection = sqlite3.connect("database.db")
-    cur = connection.cursor()
-    cur.execute(
-        "INSERT INTO exercises (name, muscle, description) VALUES (?, ?, ?)",
-        (
-            "Lat Pulldown",
-            "latissimus dorsi",
-            "The pulldown exercise works the back muscles and is performed at"
-            " a workstation with adjustable resistance, usually plates. While"
-            " seated, you pull a hanging bar toward you to reach chin level,"
-            " then release it back up with control for one repetition. This"
-            " exercise can be done as part of an upper-body strength workout.",
-        ),
-    )
 
-    cur.execute(
-        "INSERT INTO exercises (name, muscle, description) VALUES (?, ?, ?)",
-        (
-            "Wide Grip Lat Pulldown",
-            "latissimus dorsi",
-            "Wide grip version of the Lat Pulldown",
-        ),
-    )
+def init_muscle_table():
+    muscles_url = "https://wger.de/api/v2/muscle/"
+    response = requests.get(muscles_url)
+    muscles_data = response.json().get("results", [{}])
+    sorted_muscles_data = sorted(muscles_data, key=lambda d: d["id"])
+    for muscle_data in sorted_muscles_data:
+        db_utils = SqliteUtilites(DATABASE_NAME)
+        db_utils.execute(
+            "INSERT INTO muscles (muscle, muscle_latin) VALUES"
+            f" ('{muscle_data['name']}', '{muscle_data['name_en']}')",
+            commit=True,
+        )
 
-    cur.execute(
-        "INSERT INTO exercises (name, muscle, description) VALUES (?, ?, ?)",
-        (
-            "Leg Press",
-            "quadriceps",
-            "Leg presses are done in a seated position. Your legs repeatedly"
-            " press against weights, which can be adjusted according to your"
-            " fitness level. This targets your quads, glutes, hamstrings,"
-            " hips, and calves. The seated position of leg presses helps keep"
-            " your upper body and torso still.",
-        ),
-    )
-    connection.commit()
-    connection.close()
+
+def init_exercise_table():
+    # Set up images for exercises
+    image_url = "https://wger.de/api/v2/exerciseimage/?is_main=true&exercise=3"
+    image_url_response = requests.get(image_url)
+    exercises_image_data = image_url_response.json().get("results", [{}])
+
+    image_dict = {}
+    default_image = "static/images/coming_soon.jpg"
+    for exercise_image_data in exercises_image_data:
+        exercise_base = exercise_image_data.get("exercise_base")
+
+        exercise_image = exercise_image_data.get("image", default_image)
+        image_dict[exercise_base] = exercise_image
+    # Set up exercises
+    exercises_url = "https://wger.de/api/v2/exercise/?limit=1000&status=2&language=2&muscles="
+    exercises_url_response = requests.get(exercises_url)
+    exercises_data = exercises_url_response.json().get("results", [{}])
+
+    for exercise_data in exercises_data:
+        db_utils = SqliteUtilites(DATABASE_NAME)
+        exercise_name = exercise_data["name"]
+        exercise_base = exercise_data["exercise_base"]
+        exercise_muscle = (
+            exercise_data["muscles"][0]
+            if len(exercise_data["muscles"]) > 0
+            else "UNKNOWN"
+        )
+        exercise_description = f"""
+        {sanitizer(exercise_data['description'])}
+        """
+
+        exercise_image = image_dict.get(exercise_base, default_image)
+        db_utils.execute(
+            "INSERT INTO exercises (name, muscle, description, image_url)"
+            f' VALUES ("{exercise_name}", "{exercise_muscle}",'
+            f' "{exercise_description}", "{exercise_image}")',
+            commit=True,
+        )
+
+
+def sanitizer(value: str):
+    regex_pattern = re.compile("<.*?>")
+    return re.sub(regex_pattern, "", value).replace('"', "'")
