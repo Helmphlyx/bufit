01b264e5a61d89f225f6d190b967ce9d8d49b8d5
Taina Conde <tainabatistaconde@gmail.com>
2023-02-17 14:40:36 -0500

    Handle single quote when satinizing input

--- helpers.py
+++ helpers.py
@@ -1,9 +1,15 @@
 def sanitize_input(input):
     input_lower = input.lower()
     input_list = input_lower.split()
+    result_list = []
     for element in input_list:
         element.strip()
-    input_sanitized = " ".join(input_list)
+        if "'" in element:
+            print(element)
+            index = element.index("'")
+            element = element[:index] + "'" + element[index:]
+        result_list.append(element)
+    input_sanitized = " ".join(result_list)
     return input_sanitized
 def title_input(input):
     input_title = input.title()
