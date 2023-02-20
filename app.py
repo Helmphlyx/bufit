6bfddd6cb85d14e297090a5d30e229f0ebe0af43
Helmphlyx <btruong413@gmail.com>
2023-02-20 01:19:49 -0500

    role based access

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask_login import LoginManager
-from models import User
 from database import db, init_db
 from sqlalchemy_utils import database_exists
 from settings import settings
+from models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Mail
 
 global_user = None
+
+
+def create_admin():
+    admin_user = User(
+        email="bufitsite@gmail.com",
+        name="admin",
+        password=generate_password_hash("Bufitcs633", method="sha256"),
+        access=2,
+        is_confirmed=True,
+    )
+    db.session.add(admin_user)
+    db.session.commit()
 
 
 def create_app():
@@ -21,6 +35,16 @@ def create_app():
     app.config["UPLOAD_FOLDER"] = "static/images/"
     app.url_map.strict_slashes = False
 
+    mail_settings = {
+        "MAIL_SERVER": "smtp.gmail.com",
+        "MAIL_PORT": 465,
+        "MAIL_USE_TLS": False,
+        "MAIL_USE_SSL": True,
+        "MAIL_USERNAME": "bufitsite@gmail.com",
+        "MAIL_PASSWORD": "zsofxopzkulwrqnb",
+    }
+    app.config.update(mail_settings)
+
     db.init_app(app)
 
     # blueprint for auth routes in our app
@@ -32,16 +56,16 @@ def create_app():
 
     app.register_blueprint(main_blueprint)
 
-    from models import User
-
     with app.app_context():
         if not database_exists(SQLALCHEMY_DATABASE_URI_LONG):
             db.create_all()
             init_db()
+            create_admin()
     return app
 
 
 app = create_app()
+mail = Mail(app)
 login_manager = LoginManager(app)
 
 
